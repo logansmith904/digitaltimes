06752625eee158b8b88a8ea55827181559e8958f
Abdulah Mohamed <abdullahmohamed.en@gmail.com>
2024-05-06 02:10:32 +0300

    players slider and global head& footer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var swiperContainer = document.getElementsByClassName("slide-container")[0];
 var pagination = document.getElementsByClassName("swiper-pagination")[0];
 
-var swiper = new Swiper(swiperContainer, {
+var swiper = new swiper(swiperContainer, {
   slidesPerView: 3,
   spaceBetween: 30,
   pagination: {
